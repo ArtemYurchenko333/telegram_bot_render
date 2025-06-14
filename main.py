2ba6eb2d057bb3fa186afdeedd6c04cb14d5f105
ArtemYurchenko333 <106178309+ArtemYurchenko333@users.noreply.github.com>
2025-06-14 12:19:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет! Я работаю на Render 😊")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+print("Бот запущен...")
+app.run_polling()
