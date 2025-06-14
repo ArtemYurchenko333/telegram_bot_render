20b61f374cc7ce8d22c9ac6c8175269ea8055c5b
ArtemYurchenko333 <106178309+ArtemYurchenko333@users.noreply.github.com>
2025-06-14 12:20:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("7743785595:AAEiPtPZwUX2wODau3uyJzB-h0I2P2vlhI8")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Я работаю на Render 😊")
